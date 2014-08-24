6d18b2473badafe5ba5228706e470e9d75c036ff
Daniel Smith <d.lake.smith@eagles.usm.edu>
2014-08-23 23:42:28 -0500

    Update URL to fetch from heroku backend

--- app/js/services.js
+++ app/js/services.js
@@ -9,5 +9,5 @@ angular.module('myApp.services', []).
   value('version', '0.1').
 
 factory('Watchlist', function($resource) {
-	return $resource("http://localhost:9292/users/danielsmith4483/watchlist/shows");
+	return $resource("http://how-long-to-watch.herokuapp.com/users/danielsmith4483/watchlist/shows");
 });
