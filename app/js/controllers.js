f9fe9cb96bd83ca3b4358f4b6d35822915f3c76d
W738806 <d.lake.smith@eagles.usm.edu>
2014-08-25 10:50:09 -0500

    Add formatted duration description

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,15 +5,28 @@
 angular.module('myApp.controllers', [])
   .controller('watchlistController', ['$scope', 'Watchlist', function($scope, Watchlist) {
 
-  	$scope.username = "";
-
   	$scope.searchClicked = function(username) {
   		Watchlist.get({ id: username }, function(data) {
   		$scope.watchlist = data;
-
-  		console.log(data);
   	});
   	};
+
+    $scope.formattedDuration = function(show) {
+      var totalMinutes = show.episode_duration * show.episode_count;
+      var hours = Math.floor(totalMinutes / 60);
+      var minutes = totalMinutes % 60;
+      var hourDescription = (hours === 1) ? "hour" : "hours";
+      var minuteDescription = (minutes === 1) ? "minute" : "minutes";
+
+      if (minutes === 0) {
+        return hours + " " + hourDescription;
+      }
+      if (minutes === 0) {
+        return minutes + " " + minuteDescription;
+      }
+
+      return hours + " " + hourDescription + " " + minutes + " " + minuteDescription;
+    };
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
